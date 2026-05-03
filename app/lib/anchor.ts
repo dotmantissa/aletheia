e5ead75dee033ff724daaf82697d54befad39d33
dotmantissa <negativemantissa@gmail.com>
2026-05-03 14:12:04 +0100

    Fix associated token program ID for claim token account derivation

--- app/lib/anchor.ts
+++ app/lib/anchor.ts
@@ -13,7 +13,7 @@ import {
 import { PROGRAM_ID, RPC_URL } from "./constants";
 
 export const TOKEN_PROGRAM_ID = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
-export const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey("ATokenGPvbdGVxr1a7jZLS11Xnt8NTcJ7E6kB9fM9q");
+export const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
 const AUCTION_SEED = "auction";
 const BID_SEED = "bid";
 const VAULT_AUTH_SEED = "vault_auth";
